81bae60efee0467f86ace1ff76585ab309f9a4b3
Pavlo <47033358+Shevchook@users.noreply.github.com>
2019-11-27 15:27:13 +0200

    Update plot1.R

--- LAB 6/plot1.R
+++ LAB 6/plot1.R
@@ -1,1 +1,26 @@
+# Task 0
+# Підготовка даних
+# Зчитування даних і заміна "?" на "NA"
+consumption <- read.table(file = "./household_power_consumption.txt", header = TRUE,
+                          sep = ";", na.strings = "?")
 
+# Виокремлення даних за 2 дні
+TwoDays <- subset(consumption, Date == "1/2/2007" | Date == "2/2/2007")
+
+# Створення нової змінної, зліпленої з двох
+TwoDays$FullDate <- paste(TwoDays$Date, TwoDays$Time)
+
+# Зміна з тексту на формат дати
+TwoDays$FullDate <- strptime(TwoDays$FullDate, "%d/%m/%Y %H:%M:%S")
+TwoDays$Date <- as.Date(TwoDays$Date, "%d/%m/%Y")
+
+
+# Task 1
+
+# Завдання параметрів якості (кількості пікселів)
+png(filename="./plot1.png", width=480, height=480)
+# Побудова гістограми
+hist(TwoDays$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency", main = "Global Active Power")
+# Функція dev.off призначена, щоб наступний графік не записався на попередній
+dev.off()
